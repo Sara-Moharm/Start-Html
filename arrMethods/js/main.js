4cbb35a89413a0c7f8699a91bb483c1081535456
Sara-Moharm <saramoharm1@gmail.com>
2024-07-31 20:43:31 +0300

    Upload

--- arrMethods/js/main.js
+++ arrMethods/js/main.js
@@ -32,16 +32,39 @@
 
 // ====================== For Loop =============================
 
-let arr = [10, 30, 50, 80, 100];
+// let arr = [10, 30, 50, 80, 100];
 
-for (let i = 0; i <= 4; i++) {
-  console.log(arr[i]);
-}
+// for (let i = 0; i <= 4; i++) {
+//   console.log(arr[i]);
+// }
 
-console.log("\n\n");
+// console.log("\n\n");
 
-arr.push(200);
+// arr.push(200);
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// ======================== Check Salaries And Double Salary ===========================
+let salaries = [10000, 20000, 30000];
+
+let doubleSalaries = [];
+
+// for (let i = 0; i < salaries.length; i++) {
+//   doubleSalaries.push(salaries[i] * 2);
+// }
+
+// console.log(doubleSalaries);
+
+// for (let salary of salaries) {
+//   doubleSalaries.push(salary * 2);
+// }
+
+// console.log(doubleSalaries);
+
+let doubleSalary = salaries.map((salary) => {
+  return salary * 2;
+});
+
+console.log(doubleSalary);
