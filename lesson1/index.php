c482fb9702d84854bcfc5110a6b9be338857ba87
Sara-Moharm <saramoharm1@gmail.com>
2024-08-18 15:48:56 +0300

    Upload Factorial

--- lesson1/index.php
+++ lesson1/index.php
@@ -1,20 +1,29 @@
 <?php
 
-    $num1 = 200;
-    $num1 = 400;
-    $op = '+';
-    if($op === '+'){
-        $result = $num1 + $num1;
-        echo "$num1 + $num2 = $result";
-    }elseif($op === '-'){
-        $result = $num1 - $num2;
-        echo "$num1 - $num2 = $result";
-    }elseif($op === '*'){
-        $result = $num1 * $num2;
-        echo "$num1 * $num2 = $result";
-    }elseif($op === '/'){
-        $result = $num1 / $num2;
-        echo "$num1 / $num2 = $result";
-    }else{
-        echo "Invalid operator";
+    // $num1 = 200;
+    // $num2 = 400;
+    // $op = '+';
+    // if($op === '+'){
+    //     $result = $num1 + $num2;
+    //     echo "$num1 + $num2 = $result";
+    // }elseif($op === '-'){
+    //     $result = $num1 - $num2;
+    //     echo "$num1 - $num2 = $result";
+    // }elseif($op === '*'){
+    //     $result = $num1 * $num2;
+    //     echo "$num1 * $num2 = $result";
+    // }elseif($op === '/'){
+    //     $result = $num1 / $num2;
+    //     echo "$num1 / $num2 = $result";
+    // }else{
+    //     echo "Invalid operator";
+    // }
+    function factorial($num){
+        $fac = 1;
+        for($i = $num; $i >= 1; $i--){
+            $fac *= $i;
+        }
+        return $fac;
     }
+
+    echo factorial(5);
